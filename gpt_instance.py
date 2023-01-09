974410cb5f6ee12d9e04699b61ec1ac33503d61d
Maximxls <maximxlssoff@gmail.com>
2023-01-09 15:19:19 +0300

    added the instance class

---
+++ gpt_instance.py
@@ -0,0 +1,24 @@
+from conf import config
+from translator import generate_russian
+
+
+class GptInstance:
+    def __init__(self, prefix, name, prompt):
+        self.prefix = prefix
+        self.name = name
+        self.prompt = prompt
+        self.history = []
+
+    def is_triggered(self, text: str) -> bool:
+        return text.startswith(self.prefix) or self.name.lower() in text.lower()
+
+    def construct_prompt(self) -> str:
+        msgs = "\n".join(self.history)
+        return f"{self.prompt}\n{msgs}\n{self.name}: "
+
+    def generate(self) -> str:
+        while len(prompt := self.construct_prompt()) > config["limits"]["max_context"] and len(self.history) > 0:
+            self.history.pop(0)
+        output = generate_russian(prompt).strip()
+        self.history += [f"{self.name}: {output}"]
+        return output
