e5640f22217fe62bbc79dbe7656b0dd66a0f4de3
Maximxls <maximxlssoff@gmail.com>
2023-01-09 16:44:43 +0300

    added loading config from the network

--- conf.py
+++ conf.py
@@ -1,7 +1,16 @@
 import tomli
+from urllib.request import urlopen
 
 with open("config.toml", "rb") as f:
     config = tomli.load(f)
 
 with open("persons.toml", "rb") as f:
+    persons = tomli.load(f)
+
+if "config_url" in config:
+    f = urlopen(config["config_url"])
+    config = tomli.load(f)
+
+if "persons_url" in config:
+    f = urlopen(config["persons_url"])
     persons = tomli.load(f)
